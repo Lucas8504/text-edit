26cfa9b54095d38e2324233ab71c66a589ae3c22
Lucas Araujo <lsa1@live.com.ar>
2022-06-12 16:23:24 -0300

    frames y marcos

--- Tk.py
+++ Tk.py
@@ -3,8 +3,20 @@ from tkinter import *
 root = Tk()
 
 root.title("Hola mundo")
-root.resizable(1.1)
+root.resizable(1,1)
 root.iconbitmap("png-transparent.ico")
+
+frame = Frame(root, width=480, height=320)
+frame.pack(fill="y", expand=1)
+frame.config(cursor= "pirate")
+frame.config(bg="lightblue")
+frame.config(bd=25)
+frame.config(relief="sunken")
+
+root.config(cursor= "arrow")
+root.config(bg="blue")
+root.config(bd=15)
+root.config(relief="ridge")
 
 #abajo del tod0
 root.mainloop()
