95fdd81f48fb20c6648fec453f7fb88a5687975e
Lucas Araujo <lsa1@live.com.ar>
2022-06-12 15:05:14 -0300

    Tk raiz title, resizable, icon.

---
+++ Tk.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("Hola mundo")
+root.resizable(1.1)
+root.iconbitmap("png-transparent.ico")
+
+#abajo del tod0
+root.mainloop()
+# si cambiamos el nombre de la extencion .py por .pyw
+# forzaremos a que no se habra la consola de fondo
