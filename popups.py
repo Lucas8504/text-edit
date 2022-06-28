b3d90be3d634db30afc9f755e43b1826e50d856e
Lucas Araujo <lsa1@live.com.ar>
2022-06-28 20:13:52 -0300

    Pop ups II (vent. emergentes)

--- popups.py
+++ popups.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter import colorchooser
+from tkinter import filedialog
 
 
 # Configuracion de la raiz
@@ -15,10 +17,21 @@ def test():
      # resultado = messagebox.askokcancel("Salir", "¿Estas seguro que deseas salir sin guardar?")
      # if resultado:
      #   root.destroy()
+     # color = colorchooser.askcolor(title= "Elige un color")
+     # print(color)
+     # ruta = filedialog.askopenfilename(title="Abrir archivo", initialdir="c:", filetypes=(("Fichero de texto", "*.txt"), ("Archivo de textoHD", "*.txt2hd"), ("todos los ficheros", "*.*")))
+     # print(ruta)
+
+     # equivale a open (ruta, w) w predeterminado borra el archivo lo reemplazar otro
+     fichero = filedialog.asksaveasfile(title="Guardar un archivo", mode="w", defaultextension=".txt")
+     if fichero is not None:
+         fichero.write("hola")
+         fichero.close()
 
 
 
 Button(root, text="Click", command=test).pack()
+
 
 
 # Finalmente bucle de la aplicacion
