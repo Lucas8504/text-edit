21ede8e7fe50fbc8bd48c8f82afb6a5091de408a
Lucas Araujo <lsa1@live.com.ar>
2022-10-08 16:10:52 -0300

    act cambios

--- Editor.py
+++ Editor.py
@@ -1,10 +1,21 @@
 from tkinter import *
 from tkinter import filedialog
+from tkinter import messagebox
 from io import open
 
-ruta = "" # Almacena la ruta del fichero
+ruta = ""  # Almacena la ruta del fichero
 
-def Nuevo():
+def sugerencia():
+    mensaje.set("No te olvides de guardar!")
+    result = messagebox.askyesnocancel("Salir", "¿Quieres salir sin guardar?")
+    if result:
+        root.destroy()
+    else:
+        guardar()
+
+
+
+def nuevo():
     global ruta
     mensaje.set("Nuevo fichero")
     ruta = ""
@@ -12,11 +23,11 @@ def Nuevo():
     root.title("Mi editor.")
 
 
-def Abrir():
+def abrir():
     global ruta
     mensaje.set("Abrir fichero")
-    ruta = filedialog.askopenfilename(initialdir="C:",
-            filetypes=(("ficheros de texto", "*.txt"),),
+    ruta = filedialog.askopenfilename(initialdir="c:",
+            filetypes =(("ficheros de texto", "*.txt"),),
             title="Abrir un fichero de texto")
 
     if ruta != "":
@@ -24,19 +35,38 @@ def Abrir():
         contenido = fichero.read()
         texto.insert('insert', contenido)
         fichero.close()
-        root.title("Mi editor.  " + ruta)
+        root.title("Mi editor .  " + ruta)
 
 
 
-def Guardar():
+def guardar():
     mensaje.set("Guardar fichero")
+    if ruta != "":
+        contenido = texto.get(1.0,'end-1c')
+        fichero = open(ruta, 'w+')
+        fichero.write(contenido)
+        fichero.close()
+        mensaje.set("Fichero guadado!")
+    else:
+        guardar_como()
 
 
 
-def Guardar_como():
+
+def guardar_como():
+    global ruta
     mensaje.set("Guardar fichero como")
-
-
+    fichero = filedialog.asksaveasfile(title="Guardar fichero", mode="w", defaultextension=".txt")
+    if fichero is not None:
+        ruta = fichero.name
+        contenido = texto.get(1.0, 'end-1c')
+        fichero = open(ruta, 'w+')
+        fichero.write(contenido)
+        fichero.close()
+        mensaje.set("Fichero guadado!")
+    else:
+        mensaje.set("Guardado cancelado")
+        ruta = ""
 
 
 # Comfiguracion de la raiz
@@ -47,10 +77,10 @@ root.title("Mi editor")
 # Menu superior
 menubar = Menu(root)
 filemenu = Menu (menubar, tearoff=0)
-filemenu.add_command(label="Nuevo", command=Nuevo)
-filemenu.add_command(label="Abrir", command=Abrir)
-filemenu.add_command(label="Guardar", command=Guardar)
-filemenu.add_command(label="Guardar como", command=Guardar_como)
+filemenu.add_command(label="Nuevo", command=nuevo)
+filemenu.add_command(label="Abrir", command=abrir)
+filemenu.add_command(label="Guardar", command=guardar)
+filemenu.add_command(label="Guardar como", command=guardar_como)
 filemenu.add_separator()
 
 filemenu.add_command(label="Salir", command=root.quit)
@@ -64,10 +94,11 @@ texto.config(bd=0, padx=6, pady=4, font=("Consolas", 12))
 # Monitor imferior
 mensaje = StringVar()
 mensaje.set("Biemvenido a tu editor")
-monitor = Label(root, textvar=mensaje, justify="left")
+monitor = Label(root, textvariable=mensaje, justify="left")
 monitor.pack(side="left")
 
 
 root.config(menu=menubar)
 # Finalmente bucle de la aplicacion
+root.protocol("WM_DELETE_WINDOW", sugerencia)
 root.mainloop()
