cb4dacddd6970ca6cc508bd0f93b7f1999a8f043
Lucas Araujo <lsa1@live.com.ar>
2022-10-11 09:29:00 -0300

    Agreg. en gitignore y correciones Tk.py

--- Tk.py
+++ Tk.py
@@ -3,22 +3,21 @@ from tkinter import *
 root = Tk()
 
 root.title("Hola mundo")
-root.resizable(1,1)
+root.resizable(True, True)
 root.iconbitmap("png-transparent.ico")
 
 frame = Frame(root, width=480, height=320)
 frame.pack(fill="y", expand=1)
-frame.config(cursor= "pirate")
+frame.config(cursor="pirate")
 frame.config(bg="lightblue")
 frame.config(bd=25)
 frame.config(relief="sunken")
 
-root.config(cursor= "arrow")
+root.config(cursor="arrow")
 root.config(bg="blue")
 root.config(bd=15)
 root.config(relief="ridge")
 
-#abajo del tod0
+# abajo del tod0 para permitir la ejecucion de la ventana
 root.mainloop()
-# si cambiamos el nombre de la extencion .py por .pyw
-# forzaremos a que no se habra la consola de fondo
+# si cambiamos el nombre de la extencion .py por (.pyw) forzaremos a que no se habra la consola de fondo
