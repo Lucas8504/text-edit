9eabbc4e9645abda2cc3d8601371c0c9095b7560
Lucas Araujo <lsa1@live.com.ar>
2022-10-12 19:00:36 -0300

    corecciones

--- Editor.py
+++ Editor.py
@@ -5,22 +5,22 @@ from io import open
 
 ruta = ""  # Almacena la ruta del fichero
 
-def salirSinGuardar():
-    fichero = open(ruta, 'r')          # Abre el fichero y guarda la informacion en la variable
-    contenido = fichero.read()         # Lee el fichero y guarda el contenido
-    context = texto.get(1.0,'end-1c')  # Guarda el contenido del cuadro de texto
-    if context != contenido:           # Compara las variables para detectar cambios en el fichero
+
+def salirSinguardar():
+    fichero = open(ruta, 'r')  # Abre el fichero y guarda la informacion en la variable
+    contenido = fichero.read()  # Lee el fichero y guarda el contenido
+    context = texto.get(1.0, 'end-1c')  # Guarda el contenido del cuadro de texto
+    if context != contenido:  # Compara las variables para detectar cambios en el fichero
         result = messagebox.askyesnocancel("Salir", "¿Quieres salir sin guardar?")
-        if result != None:
+        if result is not None:
             if result:
                 root.destroy()
             else:
-              guardar()
+                guardar()
         else:
             pass
     else:
-      root.destroy()
-
+        root.destroy()
 
 
 def nuevo():
@@ -35,8 +35,8 @@ def abrir():
     global ruta
     mensaje.set("Abrir fichero")
     ruta = filedialog.askopenfilename(initialdir="c:",
-            filetypes =(("ficheros de texto", "*.txt"),),
-            title="Abrir un fichero de texto")
+                                      filetypes=(("ficheros de texto", "*.txt"),),
+                                      title="Abrir un fichero de texto")
 
     if ruta != "":
         fichero = open(ruta, 'r')
@@ -46,19 +46,16 @@ def abrir():
         root.title("Mi editor .  " + ruta)
 
 
-
 def guardar():
     mensaje.set("Guardar fichero")
     if ruta != "":
-        contenido = texto.get(1.0,'end-1c')
+        contenido = texto.get(1.0, 'end-1c')
         fichero = open(ruta, 'w+')
         fichero.write(contenido)
         fichero.close()
         mensaje.set("Fichero guadado!")
     else:
         guardar_como()
-
-
 
 
 def guardar_como():
@@ -81,10 +78,9 @@ def guardar_como():
 root = Tk()
 root.title("Mi editor")
 
-
 # Menu superior
 menubar = Menu(root)
-filemenu = Menu (menubar, tearoff=0)
+filemenu = Menu(menubar, tearoff=0)
 filemenu.add_command(label="Nuevo", command=nuevo)
 filemenu.add_command(label="Abrir", command=abrir)
 filemenu.add_command(label="Guardar", command=guardar)
@@ -94,7 +90,7 @@ filemenu.add_separator()
 filemenu.add_command(label="Salir", command=root.quit)
 menubar.add_cascade(menu=filemenu, label="Archivo")
 
-#Caja de texto central
+# Caja de texto central
 texto = Text(root)
 texto.pack(fill="both", expand=1)
 texto.config(bd=0, padx=6, pady=4, font=("Consolas", 12))
@@ -105,8 +101,7 @@ mensaje.set("Biemvenido a tu editor")
 monitor = Label(root, textvariable=mensaje, justify="left")
 monitor.pack(side="left")
 
-
 root.config(menu=menubar)
 # Finalmente bucle de la aplicacion
-root.protocol("WM_DELETE_WINDOW", salirSinGuardar)
+root.protocol("WM_DELETE_WINDOW", salirSinguardar)
 root.mainloop()
